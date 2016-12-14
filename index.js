cd5b6a3d5b23933dacb10b475aa5955f42397d63
xuanruntang <404622085@qq.com>
2016-12-14 17:10:33 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const updateHandler = require('./lib/updateHandler');
 const app = express();
+const port = 4567;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.post('/publish', (req, res) => {
-  res.send('GET request to the homepage');
+  res.send('publish');
 });
 
-app.post('/update', (req, res) => {
-  res.send('POST request to the homepage');
-});
+app.post('/push', updateHandler);
 
-app.listen(3000, () => {
-  console.log('Assets server start');
+app.listen(port, () => {
+  console.log('Assets Server start at ' + port);
 });
