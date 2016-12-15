1f9abe5853ffba5fdb297883714b7f45c823415a
xuanruntang <404622085@qq.com>
2016-12-15 11:47:15 +0800

    add log and ignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 require('dotenv').config();
+require('./lib/initLogger');
 const express = require('express');
 const bodyParser = require('body-parser');
 const updateHandler = require('./lib/updateHandler');
 const app = express();
+const logger = require('log4js').getLogger();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -12,12 +14,12 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.post('/publish', (req, res) => {
+app.get('/publish', (req, res) => {
   res.send('publish');
 });
 
 app.post('/push', updateHandler);
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log('Assets Server start at ' + process.env.SERVER_PORT);
+  logger.debug('Assets Server start at ' + process.env.SERVER_PORT, new Date());
 });
