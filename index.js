b989c2249ebd676c46a5b49017d2fc6673bc965d
xuanruntang <404622085@qq.com>
2016-12-16 21:21:46 +0800

    update

--- index.js
+++ index.js
@@ -19,12 +19,39 @@ app.get('/ping', (req, res) => {
   res.send('200');
 });
 
+app.post('/push', pushHandler);
+
+app.get('/saveToS3', (req, res) => {
+  const copyFiles = require('./lib/copyFiles');
+  console.log('saveToS3');
+  copyFiles({
+    reposInfo: {
+      type: 'tag',
+      ref: 'refs/heads/dev/0.0.1',
+      branch: 'dev/0.0.1',
+      cloneBranch: 'dev/0.0.1',
+      version: '0.0.1',
+      repos: {
+        name: 'InceptionPadNews-front-end',
+        ssh_url: 'git@github.com:tanlukang/InceptionPadNews-front-end.git',
+        clone_url: 'https://github.com/tanlukang/InceptionPadNews-front-end.git'
+      }
+    },
+    tmpDir: './tmp/InceptionPadNews-front-end'
+  }).then(data => {
+    logger.debug('Finished');
+    res.send('copy files finish');
+  });
+});
+
 app.get('/cloneBranch', (req, res) => {
   const cloneGithub = require('./lib/cloneGithub');
   cloneGithub({
     reposInfo: {
+      type: 'branch',
       ref: 'refs/heads/dev/0.0.1',
       branch: 'dev/0.0.1',
+      cloneBranch: 'dev/0.0.1',
       version: '0.0.1',
       repos: {
         name: 'InceptionPadNews-front-end',
@@ -40,7 +67,7 @@ app.get('/cloneBranch', (req, res) => {
       // }
     }
   }).then(data => {
-    logger.debug('Finished:');
+    logger.debug('Finished');
     res.send('clone branch finished');
   });
 });
@@ -49,7 +76,9 @@ app.get('/cloneTag', (req, res) => {
   const cloneGithub = require('./lib/cloneGithub');
   cloneGithub({
     reposInfo: {
+      type: 'tag',
       ref: 'refs/heads/publish/0.0.1',
+      cloneBranch: 'master',
       branch: 'publish/0.0.1',
       version: '0.0.1',
       repos: {
@@ -59,13 +88,10 @@ app.get('/cloneTag', (req, res) => {
       }
     }
   }).then(data => {
-    logger.debug('Finished:');
+    logger.debug('Finished');
     res.send('clone tag finished');
   });
 });
-
-
-app.post('/push', pushHandler);
 
 app.listen(process.env.SERVER_PORT, () => {
   logger.debug('Assets Server start at ' + process.env.SERVER_PORT);
